28eefe48d497caa062a7fb1c9033a88ca9a7d466
Matt Dowell <mattdowell12@gmail.com>
2026-05-14 16:42:51 -0700

    Board hits lambda

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
 Wiki: https://www.waveshare.com/wiki/ESP32-S3-ePaper-1.54
+
+Pinout: https://raw.githubusercontent.com/waveshareteam/ESP32-S3-ePaper-3.97/main/Arduino/examples/02_E-Paper_Example/DEV_Config.h
--- infra/arduino/arduino.ino
+++ infra/arduino/arduino.ino
@@ -1,37 +1,52 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include "arduino_secrets.h"  // Not version controlled because contains secrets
 
-// --- Credentials ---
 const char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS;
+const char* lambdaUrl = SECRET_LAMBDA_URL;
 
 void setup() {
-  // Use 115200 for ESP32-S3
   Serial.begin(115200);
-  
-  // Wait for Serial to initialize (crucial for ESP32-S3 USB CDC)
   while (!Serial) {
     delay(10);
   }
 
-  Serial.println("\n--- WiFi Connection Test ---");
   Serial.print("Connecting to: ");
   Serial.println(ssid);
-
-  // Start the connection
   WiFi.begin(ssid, password);
-
-  // Monitor the status
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("\nWiFi Connected!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // We'll add your AWS Lambda request here next!
+  Serial.println("Start loop");
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure client;
+    client.setInsecure();  // Skip cert validation; Lambda URL uses AWS-managed cert
+
+    HTTPClient http;
+    http.begin(client, lambdaUrl);
+
+    int httpCode = http.GET();
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println("Lambda response:");
+      Serial.println(payload);
+    } else {
+      Serial.print("HTTP error: ");
+      Serial.println(httpCode);
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi disconnected, skipping request");
+  }
+
+  delay(30000);  // Poll every 30 seconds
 }
